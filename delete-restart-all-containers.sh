fa2807f9809603e44e2a907498b5e053386e7a36
Andrew Beveridge <andrew@beveridge.uk>
2019-12-06 02:37:33 +0100

    Replaced nginx setup with traefik for much better setup overall, got homearea working

--- delete-restart-all-containers.sh
+++ delete-restart-all-containers.sh
@@ -3,27 +3,7 @@
 docker rm -f `docker ps -aq`
 docker system prune -f
 
-cd docker-compose-letsencrypt-nginx-proxy-companion
-./start.sh
-cd ..
+docker-compose up -d
 
-docker run --detach \
-    --name grafana \
-    --network=webproxy \
-    --env "VIRTUAL_HOST=grafana.beveradb.com" \
-    --env "VIRTUAL_PORT=3000" \
-    --env "LETSENCRYPT_HOST=grafana.beveradb.com" \
-    --env "LETSENCRYPT_EMAIL=andrew@beveridge.uk" \
-    grafana/grafana
-
-docker run --detach \
-    --name homearea \
-    --network=webproxy \
-    --env "VIRTUAL_HOST=homearea.beveradb.com" \
-    --env "VIRTUAL_PORT=3000" \
-    --env "LETSENCRYPT_HOST=homearea.beveradb.com" \
-    --env "LETSENCRYPT_EMAIL=andrew@beveridge.uk" \
-    homeareainfo
-
-
+docker-compose logs -f
 
